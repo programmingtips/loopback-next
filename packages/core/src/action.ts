f0ac59346a688978440c9557c65f3038116ab0f6
Raymond Feng <rfeng@us.ibm.com>
2017-09-29 16:39:58 -0700

    feat(core): Add action composition by dependencies
    The proposal introduces `@action` to decorate action classes and
    methods. It capatures the dependencies between action methods so
    that they can be sorted and invoked using method dependnecy injection.

---
+++ packages/core/src/action.ts
@@ -0,0 +1,272 @@
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {
+  Context,
+  Reflector,
+  Constructor,
+  Injection,
+  instantiateClass,
+  describeInjectedArguments,
+  describeInjectedProperties,
+} from '@loopback/context';
+
+// tslint:disable-next-line:variable-name
+const Topo = require('topo');
+
+export const ACTION_KEY = 'action';
+export const ACTION_METHODS_KEY = 'action:methods';
+
+// tslint:disable:no-any
+
+/**
+ * Metadata for actions
+ */
+export interface ActionMetadata {
+  /**
+   * Name of the action, default to the class or method name
+   */
+  name: string;
+  /**
+   * An array of events the action fulfills
+   */
+  fulfills: string[];
+  /**
+   * An array of events the action depends on
+   */
+  dependsOn: string[];
+  /**
+   * Target of the metadata. It is the class for action classes
+   * or prototype for action methods
+   */
+  target: any;
+}
+
+export interface ActionMethod extends ActionMetadata {
+  /**
+   * Name of the method
+   */
+  method: string;
+  /**
+   * Specify a key to bind the return value into the context
+   */
+  bindsReturnValueAs?: string;
+  /**
+   * The action class
+   */
+  actionClass?: ActionClass;
+}
+
+export interface ActionClass extends ActionMetadata {
+  methods: {[name: string]: ActionMethod};
+}
+
+/**
+ * Normalize the binding keys to be unique and without #
+ * @param keys An array of binding keys
+ */
+function normalizeKeys(keys?: string[]) {
+  keys = keys || [];
+  keys = keys.map(k => k.split('#')[0]);
+  keys = Array.from(new Set(keys));
+  return keys;
+}
+
+/**
+ * Populate dependsOn/fulfills into the action metadata
+ * @param actionMetadata Action metadata
+ * @param injections An array of injections
+ */
+function populateActionMetadata(
+  actionMetadata: ActionMetadata,
+  injections: Injection[],
+) {
+  // Add dependsOn from @inject
+  let dependsOn = injections
+    // Skip @inject.setter
+    .filter(p => !(p.metadata && p.metadata.setter))
+    .map(p => p.bindingKey);
+
+  dependsOn = dependsOn.concat(actionMetadata.dependsOn);
+  actionMetadata.dependsOn = normalizeKeys(dependsOn);
+
+  // Add fulfills from @inject
+  let fulfills = injections
+    // Only include @inject.setter
+    .filter(p => p.metadata && p.metadata.setter)
+    .map(p => p.bindingKey);
+
+  fulfills = fulfills.concat(actionMetadata.fulfills);
+  actionMetadata.fulfills = normalizeKeys(fulfills);
+  return actionMetadata;
+}
+
+/**
+ * Decorator for Action classes and methods, for example,
+ * ```ts
+ * @action({name: 'my-action', fulfills: ['key1'], dependsOn: ['key2']})
+ * class MyAction {
+ *   constructor(@inject('dep1') private dep1: ClassLevelDep) {}
+ *
+ *   @inject('dep2')
+ *   private dep2: PropertyLevelDep;
+ *
+ *   @action({fulfills: ['dep4']})
+ *   action1(@inject('dep3') dep3: MethodLevelDepA) {...}
+ *
+ *   @action()
+ *   action2(@inject('dep4') dep4: MethodLevelDepB) {...}
+ * }
+ * ```
+ * @param meta Action metadata
+ */
+export function action(meta?: Partial<ActionClass|ActionMethod>) {
+  return function(target: any, method?: string | symbol) {
+    let name;
+    if (method) {
+      name = `${target.constructor.name}.${method}`;
+    } else {
+      name = target.name;
+    }
+    if (meta && meta.name) {
+      name = meta.name;
+    }
+    let actionMetadata: ActionMetadata;
+    actionMetadata = method
+      ? <ActionMethod> {
+          target,
+          method,
+          name,
+          fulfills: [],
+          dependsOn: [],
+        }
+      : {
+          target,
+          name,
+          fulfills: [],
+          dependsOn: [],
+        };
+
+    if (meta && meta.fulfills) {
+      actionMetadata.fulfills = meta.fulfills;
+    }
+    if (meta && meta.dependsOn) {
+      actionMetadata.dependsOn = meta.dependsOn;
+    }
+    if (method) {
+      // Method level decoration
+
+      // First handle bindReturnValueAs
+      const methodMeta = <Partial<ActionMethod>> meta;
+      if (meta && methodMeta.bindsReturnValueAs) {
+        (<ActionMethod> actionMetadata).bindsReturnValueAs =
+          methodMeta.bindsReturnValueAs;
+        actionMetadata.fulfills.push(methodMeta.bindsReturnValueAs);
+      }
+
+      // Process method parameters
+      const injections = describeInjectedArguments(target, method);
+      populateActionMetadata(actionMetadata, injections);
+      Reflector.defineMetadata(ACTION_KEY, actionMetadata, target, method);
+
+      // Aggregate all methods for simpler retrieval
+      const actionMethods: {[p: string]: ActionMethod} =
+        Reflector.getOwnMetadata(ACTION_METHODS_KEY, target) || {};
+      actionMethods[method] = <ActionMethod> actionMetadata;
+      Reflector.defineMetadata(ACTION_METHODS_KEY, actionMethods, target);
+    } else {
+      // Class level decoration
+      const injections = describeInjectedArguments(target).concat(
+        Object.values(describeInjectedProperties(target.prototype)),
+      );
+      populateActionMetadata(actionMetadata, injections);
+      Reflector.defineMetadata(ACTION_KEY, actionMetadata, target);
+    }
+  };
+}
+
+export function inspectAction(cls: Constructor<any>) {
+  const descriptor: ActionClass = Object.assign(
+    {},
+    Reflector.getMetadata(ACTION_KEY, cls),
+  );
+  descriptor.methods = Object.assign(
+    {},
+    Reflector.getMetadata(ACTION_METHODS_KEY, cls.prototype),
+  );
+  for (const m of Object.values(descriptor.methods)) {
+    m.actionClass = descriptor;
+  }
+  return descriptor;
+}
+
+function addActionToGraph(graph: any, meta: ActionMetadata) {
+  // Add out edges for all fulfills
+  for (const p of meta.fulfills) {
+    if (graph.nodes.indexOf(p) === -1) {
+      graph.add(p, {group: p});
+    }
+  }
+  // Add in edges for all fulfills
+  for (const c of meta.dependsOn) {
+    if (graph.nodes.indexOf(c) === -1) {
+      graph.add(c, {group: c});
+    }
+  }
+  // Add method between dependsOn and fulfills
+  graph.add(meta, {
+    group: meta.name,
+    before: meta.fulfills,
+    after: meta.dependsOn,
+  });
+}
+
+/**
+ * Sort action classes based on fulfills/dependsOn
+ * @param actionClasses An array of action classes
+ * @param removeKeys Remove binding keys from the sorted result
+ */
+export function sortActionClasses(
+  actionClasses: Constructor<any>[],
+  removeKeys?: boolean,
+) {
+  const graph = new Topo();
+  for (const cls of actionClasses) {
+    const meta = Reflector.getMetadata(ACTION_KEY, cls);
+
+    addActionToGraph(graph, meta);
+  }
+  if (!removeKeys) return graph.nodes;
+  // Filter out the binding keys
+  return graph.nodes.filter((n: any) => typeof n === 'object');
+}
+
+/**
+ * Sort action methods based on fulfills/dependsOn
+ * @param actionClasses An array of action classes
+ * @param removeKeys Remove binding keys from the sorted result
+ */
+export function sortActions(
+  actionClasses: Constructor<any>[],
+  includeClasses?: boolean,
+  removeKeys?: boolean,
+) {
+  const graph = new Topo();
+  for (const cls of actionClasses) {
+    const meta = inspectAction(cls);
+    if (includeClasses) addActionToGraph(graph, meta);
+    for (const m in meta.methods) {
+      const method = meta.methods[m];
+      if (includeClasses) {
+        // Make the method depend on the class
+        method.dependsOn.push(meta.name);
+      }
+      addActionToGraph(graph, method);
+    }
+  }
+  if (!removeKeys) return graph.nodes;
+  // Filter out the binding keys
+  return graph.nodes.filter((n: any) => typeof n === 'object');
+}
---
+++ packages/core/test/unit/action.test.ts
@@ -0,0 +1,321 @@
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {expect} from '@loopback/testlab';
+import * as util from 'util';
+import {
+  Context,
+  inject,
+  Setter,
+  Reflector,
+  BindingScope,
+  invokeMethod,
+} from '@loopback/context';
+
+import {action, inspectAction, sortActions, sortActionClasses} from '../..';
+
+// tslint:disable:no-any
+describe('Action', () => {
+  let ctx: Context;
+
+  beforeEach(givenContext);
+
+  /**
+   * Mockup http header
+   */
+  interface HttpRequest {
+    url: string;
+    verb: string;
+    headers: {[header: string]: string};
+    query: {[param: string]: string};
+  }
+
+  /**
+   * StopWatch records duration for a request. There are two action methods:
+   * - start: Start the timer
+   * - stop: Stop the timer and calculates the duration
+   */
+  @action({name: 'RoundtripTimer'})
+  class StopWatch {
+    /**
+     * Start the timer (only to be invoked after http.request is set up)
+     * @param startTime Use a setter to bind `startTime`
+     */
+    @action({dependsOn: ['http.request']})
+    start(@inject.setter('startTime') startTime: Setter<Date>) {
+      startTime(new Date());
+    }
+
+    /**
+     * Calculate the duration
+     * @param startTime
+     */
+    @action({bindsReturnValueAs: 'duration', dependsOn: ['invocation']})
+    stop(@inject('startTime') startTime: Date): number {
+      return new Date().getTime() - startTime.getTime();
+    }
+  }
+
+  /**
+   * Log the tracing id and duration for a given http request
+   */
+  @action({fulfills: ['logging']})
+  class Logger {
+    /**
+     * @param prefix The logging prefix
+     */
+    constructor(@inject('log.prefix') private prefix: string) {}
+
+    /**
+     * The logging level
+     */
+    @inject('log.level') level: string = 'INFO';
+
+    private lastMessage: string; // For testing
+
+    /**
+     * Log the request tracing id and duration
+     * @param tracingId The tracing id
+     * @param duration The duration
+     */
+    @action({dependsOn: ['invocation']})
+    log(
+      @inject('tracingId') tracingId: string,
+      @inject('duration') duration: number,
+    ) {
+      this.lastMessage = util.format(
+        `[%s][%s] TracingId: %s, Duration: %d`,
+        this.level,
+        this.prefix,
+        tracingId,
+        duration,
+      );
+      console.log(this.lastMessage);
+    }
+  }
+
+  /**
+   * Set up tracing id
+   */
+  @action()
+  class Tracing {
+    /**
+     * Check and generate the tracing id for the http request
+     * @param req The http request
+     */
+    @action({bindsReturnValueAs: 'tracingId'})
+    setupTracingId(@inject('http.request') req: HttpRequest): string {
+      let id = req.headers['X-Tracing-Id'];
+      if (!id) {
+        id = req.headers['X-Tracing-Id'] = 'tracing:' + process.hrtime();
+      }
+      return id;
+    }
+  }
+
+  /**
+   * Set up http request
+   */
+  @action()
+  class HttpServer {
+    @action()
+    createRequest(
+      @inject.setter('http.request') httpRequestSetter: Setter<HttpRequest>,
+    ) {
+      httpRequestSetter({
+        verb: 'get',
+        url: 'http://localhost:3000',
+        query: {},
+        headers: {},
+      });
+    }
+  }
+
+  /**
+   * Mock-up invoker for controller methods
+   */
+  @action()
+  class MethodInvoker {
+    @action({
+      bindsReturnValueAs: 'result',
+      fulfills: ['invocation'],
+      dependsOn: ['tracingId'],
+    })
+    // FIXME(rfeng) Allow controller.name/method/args to be injected
+    invoke(): any {
+      return new Promise((resolve, reject) => {
+        // Artificially add 10ms delay to make duration significant
+        setTimeout(() => {
+          resolve('Hello, world');
+        }, 10);
+      });
+    }
+  }
+
+  it('captures class level action metadata for StopWatch', () => {
+    const meta = Reflector.getMetadata('action', StopWatch);
+    expect(meta).to.containEql({
+      target: StopWatch,
+      name: 'RoundtripTimer',
+      fulfills: [],
+      dependsOn: [],
+    });
+  });
+
+  it('captures class level action metadata for Logger', () => {
+    const meta = Reflector.getMetadata('action', Logger);
+    expect(meta).to.containEql({
+      target: Logger,
+      name: 'Logger',
+      fulfills: ['logging'],
+      dependsOn: ['log.prefix', 'log.level'],
+    });
+  });
+
+  it('captures method level action metadata', () => {
+    const meta1 = Reflector.getMetadata('action', StopWatch.prototype, 'start');
+    expect(meta1).to.eql({
+      target: StopWatch.prototype,
+      name: 'StopWatch.start',
+      method: 'start',
+      fulfills: ['startTime'],
+      dependsOn: ['http.request'],
+    });
+
+    const meta2 = Reflector.getMetadata('action', StopWatch.prototype, 'stop');
+    expect(meta2).to.eql({
+      target: StopWatch.prototype,
+      name: 'StopWatch.stop',
+      method: 'stop',
+      fulfills: ['duration'],
+      dependsOn: ['startTime', 'invocation'],
+      bindsReturnValueAs: 'duration',
+    });
+  });
+
+  it('inspects action class', () => {
+    const meta = inspectAction(StopWatch);
+    expect(meta.target).to.exactly(StopWatch);
+    expect(meta).to.containEql({
+      target: StopWatch,
+      name: 'RoundtripTimer',
+      fulfills: [],
+      dependsOn: [],
+    });
+
+    expect(meta.methods.start).to.containEql({
+      target: StopWatch.prototype,
+      name: 'StopWatch.start',
+      method: 'start',
+      fulfills: ['startTime'],
+      dependsOn: ['http.request'],
+    });
+
+    expect(meta.methods.stop).to.containEql({
+      target: StopWatch.prototype,
+      name: 'StopWatch.stop',
+      method: 'stop',
+      fulfills: ['duration'],
+      dependsOn: ['startTime', 'invocation'],
+    });
+  });
+
+  it('inspects action class with inject.setter', () => {
+    const meta = inspectAction(HttpServer);
+    expect(meta.methods['createRequest'].fulfills).to.eql(['http.request']);
+    expect(meta.methods['createRequest'].dependsOn).to.eql([]);
+  });
+
+  it('sort action classes', () => {
+    const nodes = sortActionClasses([Logger, StopWatch, HttpServer, Tracing]);
+    expect(
+      nodes.map((n: any) => (typeof n === 'object' ? 'action:' + n.name : n)),
+    ).to.eql([
+      'log.prefix',
+      'log.level',
+      'action:Logger',
+      'logging',
+      'action:RoundtripTimer',
+      'action:HttpServer',
+      'action:Tracing',
+    ]);
+  });
+
+  it('sort action methods', () => {
+    const nodes = sortActions([
+      Logger,
+      StopWatch,
+      HttpServer,
+      MethodInvoker,
+      Tracing,
+    ]);
+
+    expect(
+      nodes.map((n: any) => (typeof n === 'object' ? 'action:' + n.name : n)),
+    ).to.eql([
+      'action:HttpServer.createRequest',
+      'http.request',
+      'action:StopWatch.start',
+      'startTime',
+      'action:Tracing.setupTracingId',
+      'tracingId',
+      'action:MethodInvoker.invoke',
+      'invocation',
+      'action:StopWatch.stop',
+      'duration',
+      'action:Logger.log',
+      'result',
+    ]);
+  });
+
+  it('bind action classes', () => {
+    const classes = [Logger, StopWatch, HttpServer, MethodInvoker, Tracing];
+    const nodes = sortActionClasses(classes, true);
+    ctx.bind('log.level').to('INFO');
+    ctx.bind('log.prefix').to('LoopBack');
+    for (const c of nodes) {
+      ctx
+        .bind('actions.' + c.name)
+        .toClass(c.target)
+        .inScope(BindingScope.SINGLETON);
+    }
+
+    nodes.forEach((c: any, index: number) => {
+      const v = ctx.getSync('actions.' + c.name);
+      expect(v instanceof classes[index]).to.be.true();
+    });
+  });
+
+  it('creates a sequence of actions', async () => {
+    const actions = sortActions(
+      [Logger, StopWatch, HttpServer, MethodInvoker, Tracing],
+      true,
+      true,
+    );
+    ctx.bind('log.level').to('INFO');
+    ctx.bind('log.prefix').to('LoopBack');
+    for (const c of actions.filter((a: any) => !a.method)) {
+      ctx
+        .bind('actions.' + c.name)
+        .toClass(c.target)
+        .inScope(BindingScope.SINGLETON);
+    }
+
+    for (const m of actions.filter((a: any) => !!a.method)) {
+      const v = await ctx.get('actions.' + m.actionClass.name);
+      const result = await invokeMethod(v, m.method, ctx);
+      if (result !== undefined && m.bindsReturnValueAs) {
+        ctx.bind(m.bindsReturnValueAs).to(result);
+      }
+    }
+    const logger = await ctx.get('actions.Logger');
+    expect(logger.lastMessage).to.match(
+      /\[INFO\]\[LoopBack\] TracingId: tracing:\d+,\d+, Duration: \d+/);
+  });
+
+  function givenContext() {
+    ctx = new Context();
+  }
+});
